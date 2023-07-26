72e566190a59f6fb432c4412c86339e4e01e5c9b
Jure Vengušt <jure@equaleyes.com>
2023-07-26 15:08:37 +0200

    initial commit

---
+++ install-traefik.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wget https://github.com/traefik/traefik/releases/download/v2.10.4/traefik_v2.10.4_linux_amd64.tar.gz
+
+tar -xf traefik_v2.10.4_linux_amd64.tar.gz
+
+chmod +x traefik
+
+sudo mv traefik /usr/local/bin/traefik
+
+sudo setcap 'cap_net_bind_service=+ep' /usr/local/bin/traefik
+
+sudo groupadd -g 321 traefik
+
+sudo useradd -g traefik --no-user-group --home-dir /var/www --no-create-home --shell /usr/sbin/nologin --system --uid 321 traefik
+
+sudo mkdir /etc/traefik
+
+sudo mkdir /etc/traefik/acme
+
+sudo chown -R root:root /etc/traefik
+
+sudo chown -R traefik:traefik /etc/traefik/acme
